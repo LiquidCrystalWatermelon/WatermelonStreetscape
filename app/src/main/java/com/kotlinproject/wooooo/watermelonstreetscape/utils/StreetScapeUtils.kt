f0f4c39011043c01ecb8d6bbf4a5a125de82f9f4
Watermelon <1531997602@qq.com>
2019-05-06 13:09:19 +0800

    街景文本底层变暗； -------------------- TODO: 街景文本换行； 解决第一次有文字图片转向无文字图片时的闪屏问题； 解决小图文字看不清的问题； 识别结果本地保存； 图片网络上传； 主界面到预览界面的转场动画；

--- app/src/main/java/com/kotlinproject/wooooo/watermelonstreetscape/utils/StreetScapeUtils.kt
+++ app/src/main/java/com/kotlinproject/wooooo/watermelonstreetscape/utils/StreetScapeUtils.kt
@@ -4,8 +4,12 @@ import android.graphics.*
 import com.kotlinproject.wooooo.watermelonstreetscape.model.TranslateStreetScape
 import kotlin.math.min
 
-object StreetScapeUtils{
-    fun draw(streetScape: TranslateStreetScape):Bitmap {
+object StreetScapeUtils {
+    fun draw(
+        streetScape: TranslateStreetScape,
+        textColor: Int = Color.WHITE,
+        showDarkMask: Boolean = true
+    ): Bitmap {
         val bitmap = BitmapFactory
             .decodeFile(streetScape.photoPath)
             .copy(Bitmap.Config.ARGB_8888, true)
@@ -13,10 +17,18 @@ object StreetScapeUtils{
 //        val minSideLen = 200
         val canvas = Canvas(bitmap)
         val paint = Paint()
-        paint.color = Color.BLUE
-        paint.textSize = min(bitmap.width,bitmap.height) / 15f
+
+        if (showDarkMask) {
+            paint.color = Color.parseColor("#66000000")
+            paint.style = Paint.Style.FILL_AND_STROKE
+            canvas.drawRect(
+                0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat(), paint)
+        }
+
+        paint.color = textColor
+        paint.textSize = min(bitmap.width, bitmap.height) / 15f
         streetScape.textBoxList.forEach {
-            canvas.drawText(it.text,it.x0,it.y0,paint)
+            canvas.drawText(it.text, it.x0, it.y0, paint)
         }
         return bitmap
     }
