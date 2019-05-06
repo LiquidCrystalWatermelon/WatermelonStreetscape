941f6bc3a7a821d135813524ce22424df411d2d6
Watermelon <1531997602@qq.com>
2019-05-06 20:09:48 +0800

    长按文本弹框； -------------------- TODO: 街景文本换行； 解决小图文字看不清的问题； 识别结果本地保存； 图片网络上传； 主界面到预览界面的转场动画；

--- app/src/main/java/com/kotlinproject/wooooo/watermelonstreetscape/activity/PhotoViewActivity.kt
+++ app/src/main/java/com/kotlinproject/wooooo/watermelonstreetscape/activity/PhotoViewActivity.kt
@@ -1,5 +1,7 @@
 package com.kotlinproject.wooooo.watermelonstreetscape.activity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,12 +9,14 @@ import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewAnimationUtils
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.kotlinproject.wooooo.watermelonstreetscape.R
 import com.kotlinproject.wooooo.watermelonstreetscape.model.TranslateStreetScape
 import com.kotlinproject.wooooo.watermelonstreetscape.utils.StreetScapeUtils
 import kotlinx.android.synthetic.main.activity_photo_view.*
 import kotlin.concurrent.thread
+import kotlin.math.sqrt
 
 class PhotoViewActivity : AppCompatActivity() {
     private val TAG = "PhotoViewActivity_Log"
@@ -41,11 +45,52 @@ class PhotoViewActivity : AppCompatActivity() {
         siv_photo_view_with_text.setOnClickListener { showNoText() }
         siv_photo_view_no_text.setOnClickListener { showWithText() }
 
+        fun viewCenterR(view: View): Triple<Int, Int, Float> {
+            val w = view.width
+            val h = view.height
+            val x = w / 2
+            val y = h / 2
+            val r = sqrt((w * w + h * h).toFloat())
+            return Triple(x, y, r)
+        }
+
+        view_card_mask.setOnClickListener {
+            val (x, y, r) = viewCenterR(card_info)
+            ViewAnimationUtils
+                .createCircularReveal(card_info, x, y, r, 0f)
+                .apply {
+                    addListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(p0: Animator?) {
+                            rl_card.visibility = View.GONE
+                        }
+                    })
+                }
+                .setDuration(500)
+                .start()
+        }
+
         val gesture = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
             override fun onLongPress(e: MotionEvent?) {
                 if (e == null) return
-                val pointf = siv_photo_view_with_text.viewToSourceCoord(e.x, e.y)!!
-                Log.i(TAG, ":position ${pointf.x} ${pointf.y}")
+                val (x, y) = siv_photo_view_with_text
+                    .viewToSourceCoord(e.x, e.y)!!
+                    .let { it.x to it.y }
+                streetScape
+                    .textBoxList
+                    .filter { x in it.x0..it.x1 && y in it.y0..it.y1 }
+                    .joinToString("\n\n") { it.text }
+                    .let {
+                        if (it.isNotBlank()) {
+                            rl_card.visibility = View.VISIBLE
+                            tv_card.text = it
+                            val (cx, cy, r) = viewCenterR(card_info)
+                            Log.i(TAG, ":card $cx $cy $r")
+                            ViewAnimationUtils
+                                .createCircularReveal(card_info, cx, cy, 0f, r)
+                                .setDuration(500)
+                                .start()
+                        }
+                    }
             }
         })
 
