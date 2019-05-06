1e81ea2b542c2dd9bab9dea3c8c38dace45eb2d9
Watermelon <1531997602@qq.com>
2019-05-06 12:53:11 +0800

    有文字和无文字的街景图片互相切换不闪退了； -------------------- TODO: 街景文本底层变暗； 解决第一次有文字图片转向无文字图片时的闪屏问题； 解决小图文字看不清的问题； 街景文本换行； 识别结果本地保存； 图片网络上传； 主界面到预览界面的转场动画；

--- app/src/main/java/com/kotlinproject/wooooo/watermelonstreetscape/activity/PhotoViewActivity.kt
+++ app/src/main/java/com/kotlinproject/wooooo/watermelonstreetscape/activity/PhotoViewActivity.kt
@@ -3,6 +3,7 @@ package com.kotlinproject.wooooo.watermelonstreetscape.activity
 import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.kotlinproject.wooooo.watermelonstreetscape.R
 import com.kotlinproject.wooooo.watermelonstreetscape.model.TranslateStreetScape
@@ -12,7 +13,6 @@ import kotlinx.android.synthetic.main.activity_photo_view.*
 class PhotoViewActivity : AppCompatActivity() {
     private lateinit var streetScape: TranslateStreetScape
     private lateinit var textBitmap: Bitmap
-    private var isShowText = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,20 +28,28 @@ class PhotoViewActivity : AppCompatActivity() {
 
         textBitmap = StreetScapeUtils.draw(streetScape)
 
-        siv_photo_view.setMinimumDpi(40)
+        siv_photo_view_with_text.setMinimumDpi(40)
+        siv_photo_view_no_text.setMinimumDpi(40)
+        siv_photo_view_with_text.setImage(ImageSource.bitmap(textBitmap))
+        siv_photo_view_no_text.setImage(ImageSource.uri(streetScape.getPhotoUri(this)))
+
         showWithText()
-        siv_photo_view.setOnClickListener {
-            isShowText = !isShowText
-            if (isShowText) showWithText() else showNoText()
-        }
+        siv_photo_view_with_text.setOnClickListener { showNoText() }
+        siv_photo_view_no_text.setOnClickListener { showWithText() }
     }
 
     private fun showWithText() {
-        siv_photo_view.setImage(ImageSource.bitmap(textBitmap))
+        val (scale, center) = siv_photo_view_no_text.let { it.scale to it.center }
+        siv_photo_view_with_text.setScaleAndCenter(scale, center)
+        siv_photo_view_with_text.visibility = View.VISIBLE
+        siv_photo_view_no_text.visibility = View.GONE
     }
 
     private fun showNoText() {
-        siv_photo_view.setImage(ImageSource.uri(streetScape.getPhotoUri(this)))
+        val (scale, center) = siv_photo_view_with_text.let { it.scale to it.center }
+        siv_photo_view_no_text.setScaleAndCenter(scale, center)
+        siv_photo_view_with_text.visibility = View.GONE
+        siv_photo_view_no_text.visibility = View.VISIBLE
     }
 
     companion object {
